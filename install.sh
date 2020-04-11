a108f97f1ffef73c79812a04713363f2ce59aca6
Stefano Pace <stefanopace01@gmail.com>
2020-04-11 12:35:44 +0200

    added script to create symlinks

--- install.sh
+++ install.sh
@@ -1,1 +1,8 @@
-sudo apt install $(cat programs | tr "\n" " ")
+#sudo apt install $(cat programs | tr "\n" " ")
+
+while read link; do
+  dir=$(echo $link | sed -n -r "s|[^ ]+ ([^ ]+)/[^ ]+$|\1|p" | sed "s|~|$HOME|")
+  expanded=$(echo $link | sed "s|~|$HOME|")
+  mkdir -p $dir
+  ln -sfv $(pwd)/$expanded
+done <links
